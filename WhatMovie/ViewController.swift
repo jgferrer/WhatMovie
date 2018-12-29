949d6de8b6d1235036fe2eb862026ecfa1a1923c
José Gabriel Ferrer Morales <jgabriel.ferrer@gmail.com>
2018-12-29 11:09:11 +0100

    Initial Commit

---
+++ WhatMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatMovie
+//
+//  Created by Jose Gabriel Ferrer on 29/12/2018.
+//  Copyright © 2018 Jose Gabriel Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
